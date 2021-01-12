f4d3dca7fbdb1cd2119394a96724db8d6ea1ae82
Jessica Embury <jessemb@outlook.com>
2021-01-12 09:27:04 -0800

    untrack my_tokens.py

--- sage_geocoding/my_tokens.py
+++
@@ -1,8 +0,0 @@
-# Jessica Embury
-# RedID 823859615
-# GEOG-582 Final Project
-
-# tokens for geocoding services
-arcgis_token = '7kUouUP8Hzoaq2A51HYsza5jo4E0sgx0Rk7Xx44oBf34dAVctoy_OtXrwHbR22ktiRptnMuKPK5v4dD7wwneKvJQJhRxJV4Eh8r6SQ-wnIQBzdhmUvpztlqzfQR7FY5xxAA7IXlk-CUOtzZTDLYbpjUtUGsV-WQj7-nQdbCo74HqqDCjqhJyi4WB21u-6j_ONHKRCSE9wLLH02tP4bSaigs_tqIdf-Lkd7peEkaS8Ms.'
-bing_token = 'AitrkLp2RsjwaiTl0n-6IEZDxa5bGLdOk5VQw10CxkLw57XLLSyyZNZGf4E-E1LG'
-google_token = 'AIzaSyBSg2wDUgWl3FUZmICiK6lRlJbodirQMT4'
